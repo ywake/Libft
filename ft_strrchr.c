0f26d50c7cf2ec1448026629862b1345f616c744
ywake <ywake@student.42tokyo.jp>
2020-06-24 16:39:43 +0900

    ft_strrchr.c [c,n,t OK] fix range of while

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/23 16:57:15 by ywake             #+#    #+#             */
-/*   Updated: 2020/06/23 18:07:39 by ywake            ###   ########.fr       */
+/*   Updated: 2020/06/24 16:37:50 by ywake            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strrchr(const char *s, int c)
 
 	lensrc = ft_strlen(s);
 	i = 0;
-	while (lensrc - i > 0)
+	while (lensrc - i > -1)
 	{
 		if (s[lensrc - i] == c)
 			return ((char *)(s + lensrc - i));
