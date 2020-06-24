10663236b202382d68fa22d24d15c419633e89c3
ywake <ywake@student.42tokyo.jp>
2020-06-24 21:33:04 +0900

    ft_memchr.c [c,n,t OK]

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/24 17:40:47 by ywake             #+#    #+#             */
+/*   Updated: 2020/06/24 17:55:22 by ywake            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	char	*csrc;
+
+	csrc = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (csrc[i] == c)
+			return ((void *)&csrc[i]);
+		i++;
+	}
+	return (NULL);
+}
