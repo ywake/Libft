fa61b77580d9b20704bada5a4fd8dc548adba03d
ywake <ywake@student.42tokyo.jp>
2020-06-27 05:47:34 +0900

    ft_calloc.c [c,n,t OK]

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 14:16:53 by ywake             #+#    #+#             */
-/*   Updated: 2020/06/27 04:22:59 by ywake            ###   ########.fr       */
+/*   Updated: 2020/06/27 05:04:53 by ywake            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ int		ft_isspace(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
