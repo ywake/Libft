9e649dcf26b160b42630da885262d8afca759d9a
ywake <ywake@student.42tokyo.jp>
2020-06-29 03:38:03 +0900

    ft_lstiter.c [c,n,t,l OK]

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 14:16:53 by ywake             #+#    #+#             */
-/*   Updated: 2020/06/29 03:03:58 by ywake            ###   ########.fr       */
+/*   Updated: 2020/06/29 03:20:06 by ywake            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,7 @@ t_list			*ft_lstlast(t_list *lst);
 void			ft_lstadd_back(t_list **lst, t_list *new);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void *));
+void			ft_lstiter(t_list *lst, void (*f)(void *));
 
 int				ft_isupper(int c);
 int				ft_islower(int c);
