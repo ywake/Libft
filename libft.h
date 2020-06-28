c4ff7b33dfd979861150d0dbb00dc83c93376bc4
ywake <ywake@student.42tokyo.jp>
2020-06-28 18:46:39 +0900

    ft_lstlast.c [c,n,t,l OK]

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 14:16:53 by ywake             #+#    #+#             */
-/*   Updated: 2020/06/28 17:57:45 by ywake            ###   ########.fr       */
+/*   Updated: 2020/06/28 18:26:36 by ywake            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,7 @@ void			ft_putnbr_fd(int n, int fd);
 t_list			*ft_lstnew(void *content);
 void			ft_lstadd_front(t_list **lst, t_list *new);
 int				ft_lstsize(t_list *lst);
+t_list			*ft_lstlast(t_list *lst);
 
 int				ft_isupper(int c);
 int				ft_islower(int c);
