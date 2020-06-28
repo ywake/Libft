bcc534604098cc49a3c173a6595e06fea87e0960
ywake <ywake@student.42tokyo.jp>
2020-06-28 12:09:58 +0900

    ft_strmapi.c [c,n,t,l OK]

---
+++ ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/28 11:50:53 by ywake             #+#    #+#             */
+/*   Updated: 2020/06/28 11:58:25 by ywake            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*buf;
+
+	i = (unsigned int)ft_strlen(s);
+	buf = (char *)ft_calloc(i + 1, sizeof(char));
+	if (buf == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		buf[i] = f(i, s[i]);
+		i++;
+	}
+	return (buf);
+}
