73b61878b9cfbdac7202589aa960efc24d18851c
ywake <ywake@student.42tokyo.jp>
2020-06-30 14:05:26 +0900

    rm ft_min(), add min()

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,20 @@
 /*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/27 06:24:49 by ywake             #+#    #+#             */
-/*   Updated: 2020/06/27 07:05:39 by ywake            ###   ########.fr       */
+/*   Updated: 2020/06/30 12:49:14 by ywake            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+static size_t	min(size_t a, size_t b)
+{
+	if (a > b)
+		return (b);
+	return (a);
+}
+
+char			*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*buf;
 	size_t	bufsize;
@@ -20,12 +27,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	bufsize = 0;
 	if (ft_strlen(s) > start)
-		bufsize = ft_min(ft_strlen(s) - start, len);
+		bufsize = min(ft_strlen(s) - start, len);
 	buf = (char *)ft_calloc(bufsize + 1, sizeof(char));
 	if (buf == NULL)
 		return (NULL);
 	i = 0;
-	while (s[start + i] && i < bufsize)
+	while (i < bufsize)
 	{
 		buf[i] = s[start + i];
 		i++;
