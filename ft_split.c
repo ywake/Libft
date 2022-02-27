cb47c853aa1fc58ec53a412fb3e9eba306f67799
ywake <ywake@student.42tokyo.jp>
2022-02-27 12:28:43 +0900

    Fix: split to be short and readable.

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ywake <ywake@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/27 17:25:26 by ywake             #+#    #+#             */
-/*   Updated: 2021/06/17 22:07:22 by ywake            ###   ########.fr       */
+/*   Updated: 2022/02/27 12:27:34 by ywake            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,31 +27,6 @@ static char	**free_all(char **buf)
 	return (NULL);
 }
 
-static char	**cut_word(char const *s, char c, char **buf)
-{
-	const char	*ptr;
-	size_t		len;
-	size_t		i;
-
-	i = 0;
-	while (buf != NULL && *s)
-	{
-		ptr = ft_strchr(s, c);
-		if (ptr == NULL)
-			ptr = s + ft_strlen(s);
-		len = ptr - s;
-		if (len > 0)
-		{
-			buf[i] = ft_substr(s, 0, len);
-			if (buf[i++] == NULL)
-				buf = free_all(buf);
-		}
-		s += len + (s[len] != '\0');
-	}
-	buf[i] = NULL;
-	return (buf);
-}
-
 char	**ft_split(char const *s, char c)
 {
 	char		**buf;
@@ -65,8 +40,18 @@ char	**ft_split(char const *s, char c)
 	while (*ptr)
 		if (*ptr++ != c && (*ptr == c || *ptr == '\0'))
 			i++;
-	buf = (char **)malloc(sizeof(char *) * i);
-	if (!buf)
-		return (NULL);
-	return (cut_word(s, c, buf));
+	buf = (char **)ft_calloc(i, sizeof(char *));
+	i = 0;
+	while (s != NULL && *s != '\0' && buf != NULL)
+	{
+		ptr = ft_strchr(s, c);
+		if (ptr == NULL)
+			buf[i] = ft_strdup(s);
+		else if (ptr != s)
+			buf[i] = ft_substr(s, 0, ptr - s);
+		if (ptr != s && buf[i++] == NULL)
+			buf = free_all(buf);
+		s = ptr + (ptr != NULL);
+	}
+	return (buf);
 }
